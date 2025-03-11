a06de11e251896604bb8b18838e6b789707ac764
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-11 09:47:32 +0100

    made functions more verbose

--- Module_01/ex06/Harl.cpp
+++ Module_01/ex06/Harl.cpp
@@ -6,24 +6,32 @@ Harl::Harl() {}
 
 Harl::~Harl() {}
 
-void	Harl::_debug( void ) const
+void Harl::_debug( void ) const
 {
-	std::cout << "Debug" << std::endl;
+	std::cout << "🐛 :" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!";
+	std::cout << std::endl;
 }
 
-void	Harl::_info( void ) const
+void Harl::_info( void ) const
 {
-	std::cout << "Info" << std::endl;
+	std::cout << "ℹ️  :" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+	std::cout << std::endl;
 }
 
-void	Harl::_warning( void ) const
+void Harl::_warning( void ) const
 {
-	std::cout << "Warning" << std::endl;
+	std::cout << "⚠️  :" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.";
+	std::cout << std::endl;
 }
 
-void	Harl::_error( void ) const
+void Harl::_error( void ) const
 {
-	std::cout << "Error" << std::endl;
+	std::cout << "🚨 :" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
+	std::cout << std::endl;
 }
 
 void	Harl::_ramble( void ) const
@@ -35,10 +43,10 @@ void	Harl::filteredComplain( const std::string level ) const
 {
 	typedef	void(Harl::*FunctionPointer) () const;
 	FunctionPointer MembFunctions[5] = {
-		&Harl::_debug, 
-		&Harl::_info, 
-		&Harl::_warning, 
-		&Harl::_error, 
+		&Harl::_debug,
+		&Harl::_info,
+		&Harl::_warning,
+		&Harl::_error,
 		&Harl::_ramble
 	};
 	std::string FktNames[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
