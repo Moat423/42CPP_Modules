95ea118cca9e36e6e70a3d2d6ad67ab3c91db265
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-03-17 11:25:05 +0100

    formated division, copied files

---
+++ Module_02/ex03/Fixed.cpp
@@ -0,0 +1,97 @@
+#include "Fixed.hpp"
+/*#include <iostream>*/
+#include <cmath>
+
+const int	Fixed::_fractional = 8;
+
+// round function of my own static to only this file
+static double myRound(double value)
+{
+    return (value >= 0.0) ? floor(value + 0.5) : ceil(value - 0.5);
+}
+
+Fixed::Fixed( void ):  _value(0)
+{
+	/*std::cout << "Default constructor called" << std::endl;*/
+}
+
+Fixed::Fixed( const int value ): _value(value << this->_fractional)
+{
+	/*std::cout << "Int constructor called" << std::endl;*/
+}
+
+Fixed::Fixed( const float value ): _value(static_cast<int>(myRound(value * (1 << this->_fractional))))
+{
+	/*std::cout << "Float constructor called" << std::endl;*/
+}
+
+Fixed::~Fixed( void ) 
+{
+	/*std::cout << "Destructor called" << std::endl;*/
+}
+
+Fixed::Fixed( const Fixed &rhs )
+{
+	/*std::cout << "Copy constructor called" << std::endl;*/
+	this->setRawBits(rhs.getRawBits());
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	/*std::cout << "getRawBits member function called" << std::endl;*/
+	return (this->_value);
+}
+
+void	Fixed::setRawBits( int const raw)
+{
+	this->_value = raw;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return static_cast<float>( this->getRawBits() ) / ( 1 << _fractional );
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (this->_value >> this->_fractional);
+}
+
+Fixed	&Fixed::min( Fixed &lhs, Fixed &rhs )
+{
+	if (lhs.getRawBits() < rhs.getRawBits())
+		return (lhs);
+	else
+	 	return (rhs);
+}
+
+const Fixed	&Fixed::min( const Fixed &lhs, const Fixed &rhs )
+{
+	if (lhs.getRawBits() < rhs.getRawBits())
+		return (lhs);
+	else
+	 	return (rhs);
+}
+
+Fixed	&Fixed::max( Fixed &lhs, Fixed &rhs )
+{
+	if (lhs.getRawBits() > rhs.getRawBits())
+		return (lhs);
+	else
+	 	return (rhs);
+}
+
+const Fixed	&Fixed::max( const Fixed &lhs, const Fixed &rhs )
+{
+	if (lhs.getRawBits() > rhs.getRawBits())
+		return (lhs);
+	else
+	 	return (rhs);
+}
+
+Fixed Fixed::fromRawBits(int rawBits)
+{
+	Fixed result;
+	result._value = rawBits;
+	return result;
+}
---
+++ Module_02/ex03/Fixed.hpp
@@ -0,0 +1,52 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+
+
+class Fixed
+{
+	private:
+		int					_value;
+		static const int	_fractional;
+
+	public:
+		Fixed();
+		Fixed( const int value );
+		Fixed( const float value );
+		~Fixed();
+		Fixed( const Fixed &rhs );
+		Fixed	&operator=( const Fixed &rhs );
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+		bool	operator>(const Fixed &rhs) const;
+		bool	operator<(const Fixed &rhs) const;
+		bool	operator>=(const Fixed &rhs) const;
+		bool	operator<=(const Fixed &rhs) const;
+		bool	operator==(const Fixed &rhs) const;
+		bool	operator!=(const Fixed &rhs) const;
+		Fixed	&operator+=(const Fixed &rhs);
+		Fixed	&operator-=(const Fixed &rhs);
+		Fixed	&operator*=(const Fixed &rhs);
+		Fixed	&operator/=(const Fixed &rhs);
+		Fixed	operator+(const Fixed &rhs) const;
+		Fixed	operator-(const Fixed &rhs) const;
+		Fixed	operator*(const Fixed &rhs) const;
+		Fixed	operator/(const Fixed &rhs) const;
+		Fixed	&operator++( void ); // ++precrement
+		Fixed	operator++ ( int ); // postcrement++ with dummy int
+		Fixed	&operator--( void ); // --precrement
+		Fixed	operator-- ( int ); // postcrement-- with dummy int
+		static Fixed	&min( Fixed &lhs, Fixed &rhs );
+		static const Fixed	&min( const Fixed &lhs, const Fixed &rhs );
+		static Fixed	&max( Fixed &lhs, Fixed &rhs );
+		static const Fixed	&max( const Fixed &lhs, const Fixed &rhs );
+    // Create from raw bits (static factory method) this is only added for the tests
+		static Fixed fromRawBits(int rawBits);
+};
+
+std::ostream &operator<<( std::ostream &out, const Fixed &rhs );
+
+#endif // FIXED_H
