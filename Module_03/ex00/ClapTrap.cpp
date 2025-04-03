c4460edd72c50ae2501ef3b26d204da4d6112ee8
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-03 15:19:15 +0200

    corrected repair

--- Module_03/ex00/ClapTrap.cpp
+++ Module_03/ex00/ClapTrap.cpp
@@ -1,21 +1,33 @@
 #include "ClapTrap.hpp"
+#include "Debug.hpp"
 #include <iostream>
 #include <ostream>
 
 ClapTrap::ClapTrap( void ):
 	_name("default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
-{}
+{
+	debug("Default constructor called");
+}
 
-ClapTrap::ClapTrap( const char *name ): _name(name) {}
+ClapTrap::ClapTrap( const char *name ):
+	_name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	debug("Name constructor called");
+}
 
 ClapTrap::ClapTrap( const ClapTrap &rhs ):
 	_name(rhs._name), 
 	_hitPoints(rhs._hitPoints),
 	_energyPoints(rhs._energyPoints), 
 	_attackDamage(rhs._attackDamage)
-{}
+{
+	debug("Copy constructor called");
+}
 
-ClapTrap::~ClapTrap() {}
+ClapTrap::~ClapTrap()
+{
+	debug("Destructor called");
+}
 
 ClapTrap &ClapTrap::operator=( const ClapTrap &rhs ){
 	this->_name = rhs._name;
@@ -60,7 +72,7 @@ void ClapTrap::takeDamage( unsigned int amount )
 	}
 	this->_energyPoints--;
 	tmp_hp = this->_hitPoints - amount;
-	this->_hitPoints = tmp_hp >= 0 ? tmp_hp : 0;
+	this->_hitPoints = tmp_hp > 0 ? tmp_hp : 0;
 	std::cout << "ClapTrap " << (this->_name ? _name : "null") <<
 		" got damaged for " << amount <<
 		" and has " << this->_hitPoints <<
@@ -78,8 +90,8 @@ void ClapTrap::beRepaired( unsigned int amount )
 		return ;
 	}
 	this->_energyPoints--;
-	tmp_hp = this->_hitPoints - amount;
-	this->_hitPoints = tmp_hp >= 0 ? tmp_hp : 0;
+	tmp_hp = this->_hitPoints + amount;
+	this->_hitPoints = tmp_hp >= 10 ? 10 : tmp_hp;
 	std::cout << "ClapTrap " << (this->_name ? _name : "null") <<
 		" got repaired for " << amount <<
 		" and has " << this->_hitPoints <<
