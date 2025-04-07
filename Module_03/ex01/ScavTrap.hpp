09021ad6c61eabac0fb3827fe56fc24094e606c5
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-07 16:24:38 +0200

    this done

--- Module_03/ex01/ScavTrap.hpp
+++ Module_03/ex01/ScavTrap.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 # ifndef SCAV_MAX_HP
-#  define SCAV_MAX_HP 10
+#  define SCAV_MAX_HP 100
 # endif // SCAV_MAX_HP
 
 #include "ClapTrap.hpp"
@@ -12,15 +12,14 @@
 class ScavTrap : public ClapTrap
 {
 	public:
-		void	attack(const std::string& target);
-		const char	*getName(void);
-		void	guardGate(void) const;
-	private:
 		ScavTrap();
 		ScavTrap(const char *name);
 		~ScavTrap();
 		ScavTrap(const ScavTrap &rhs);
 		ScavTrap &operator=(const ScavTrap &rhs);
+		void	attack(const std::string& target);
+		void	guardGate(void) const;
+	private:
 };
 
 #endif // SCAVTRAP_HPP
--- Module_03/ex01/main.cpp
+++ Module_03/ex01/main.cpp
@@ -1,31 +1,41 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include <iostream>
+#include <ostream>
+#include "Debug.hpp"
 
 int	main( void )
 {
 	ClapTrap	dummy;
 	ClapTrap	npc("npc");
-	ClapTrap	helper(dummy);
-	ScavTrap	Paul(dummy);
-	dummy.attack(npc.getName());
-	npc.takeDamage(0);
-	npc.beRepaired(0);
-	npc.beRepaired(5);
-	dummy.attack(helper.getName());
-	helper.takeDamage(0);
+	ScavTrap	SC4V_TRAP;
+	ScavTrap	paul("Paul");
+	ScavTrap	SC4V_TRAP2(SC4V_TRAP);
+	//testing for correct destruction
+	ClapTrap	*pretender = new ScavTrap("pretender");
+	ClapTrap	mutant(*pretender);
+	delete pretender;
+	mutant.beRepaired(100);
+	mutant.attack("himself");
 	for (int i = 0; i < 10; i++)
 	{
-		dummy.attack(npc.getName());
+		dummy.attack("his imagination");
 		npc.takeDamage(0);
 	}
 	dummy.beRepaired(5);
-	
-	npc.beRepaired(9);
-	for (int i = 0; i < 9; i++)
-	{
+	dummy.attack("one too many times");
+	for (int i = 0; i < 11; i++)
 		npc.beRepaired(1);
+	std::cout << std::endl;
+	std::cout << CYN << "the ScavTraps have had enough and now it's their turn!!" << RESET << std::endl;
+	for (int i = 0; i < 50; i++)
+	{
+		SC4V_TRAP.attack("his imagination WITH RANGE");
+		mutant.attack("flailing arround");
+		paul.takeDamage(0);
 	}
-	//testing for correct destruction
-	    Base* obj = new Derived();
-    delete obj; // Output: Derived Destructor -> Base Destructor
+	SC4V_TRAP.beRepaired(5);
+	SC4V_TRAP.attack("one too many times WITH RANGE");
+	for (int i = 0; i < 11; i++)
+		paul.beRepaired(1);
 }
