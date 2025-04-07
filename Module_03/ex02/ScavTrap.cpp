c3740b9d899a1687c93b3ede61443a1639949a3c
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-04-07 17:41:41 +0200

    doing frag

---
+++ Module_03/ex02/ScavTrap.cpp
@@ -0,0 +1,93 @@
+#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
+#include "Debug.hpp"
+#include <iostream>
+#include <ostream>
+
+ScavTrap::ScavTrap( void ): ClapTrap()
+{
+	_hitPoints = SCAV_MAX_HP;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	debug("[ ScavTrap ] constructed");
+}
+
+ScavTrap::ScavTrap( const char *name ): ClapTrap(name)
+{
+	_hitPoints = SCAV_MAX_HP;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	debug("[ ScavTrap ] constructed with name");
+}
+
+ScavTrap::ScavTrap( const ScavTrap &rhs ): ClapTrap(rhs)
+{
+	/*_hitPoints = rhs._hitPoints;*/
+	/*_energyPoints = rhs._energyPoints;*/
+	/*_attackDamage = rhs._attackDamage;*/
+	debug("[ ScavTrap ] copy constructed");
+}
+
+ScavTrap::~ScavTrap()
+{
+	debug("[ ScavTrap ] destroyed");
+}
+
+ScavTrap &ScavTrap::operator=( const ScavTrap &rhs )
+{
+	if (this != &rhs)
+	{
+		_name = rhs._name;
+		_attackDamage = rhs._attackDamage;
+		_energyPoints = rhs._energyPoints;
+		_hitPoints = rhs._hitPoints;
+	}
+	return (*this);
+}
+
+void	ScavTrap::attack(const std::string& target)
+{
+	if (_energyPoints == 0)
+	{
+		std::cout << BLUE <<
+		"[ ScavTrap ] " << _name << " is exhausted and can't attack anymore!" <<
+		RESET << std::endl;
+		return ;
+	}
+	if (_hitPoints == 0)
+	{
+		std::cout << BRED <<
+		"[ ScavTrap ] " << _name << " is dead and can't attack anymore!" <<
+		RESET << std::endl;
+		return ;
+	}
+	_energyPoints--;
+	std::cout << RED <<
+		"[ ScavTrap ] " << (_name ? _name : "null")  <<
+		" attacks " << target <<
+		", causing " << _attackDamage  <<
+		" points of damage!" <<
+		RESET << std::endl;
+}
+
+void	ScavTrap::guardGate(void) const
+{
+	if (_energyPoints == 0)
+	{
+		std::cout << BLUE <<
+		"[ ScavTrap ] " << _name << " is exhausted and can't guard anymore!" <<
+		RESET << std::endl;
+		return ;
+	}
+	if (_hitPoints == 0)
+	{
+		std::cout << BRED <<
+		"[ ScavTrap ] " << _name << " is dead and can't guard anymore!" <<
+		RESET << std::endl;
+		return ;
+	}
+	std::cout << MAG <<
+		"[ ScavTrap ] " << _name  <<
+		" is now in GATE KEEPER MODE!" <<
+		RESET << std::endl;
+}
---
+++ Module_03/ex02/main.cpp
@@ -0,0 +1,47 @@
+#include "Debug.hpp"
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+#include <iostream>
+
+int	main( void )
+{
+	ClapTrap	dummy;
+	ClapTrap	npc("npc");
+	ScavTrap	SC4V_TRAP;
+	ScavTrap	paul("Paul");
+	//testing for correct destruction
+	ClapTrap	*pretender = new FragTrap("pretender");
+	ClapTrap	mutant(*pretender);
+	FragTrap	anon;
+	FragTrap	FR4G_TRAP(anon);
+	pretender->takeDamage(50);
+	pretender->beRepaired(30);
+	pretender->beRepaired(30);
+	pretender->takeDamage(100);
+	pretender->attack("nothing");
+	pretender->takeDamage(80);
+	pretender->beRepaired(20);
+	delete pretender;
+	anon.attack("a tumpling fly");
+	anon.takeDamage(4);
+	anon.highFivesGuys();
+	FR4G_TRAP.highFivesGuys();
+	FR4G_TRAP.beRepaired(80);
+	mutant.takeDamage(20);
+	dummy.attack("his imagination");
+	npc.takeDamage(0);
+	dummy.beRepaired(5);
+	dummy.attack("again");
+	for (int i = 0; i < 11; i++)
+		npc.beRepaired(1);
+
+	std::cout << std::endl;
+	std::cout << "the ScavTraps have had enough and now it's their turn!!" << RESET << std::endl;
+	SC4V_TRAP.attack("his imagination WITH RANGE");
+	paul.takeDamage(0);
+	SC4V_TRAP.beRepaired(5);
+	SC4V_TRAP.attack("one too many times WITH RANGE");
+	paul.beRepaired(1);
+	SC4V_TRAP.guardGate();
+}
