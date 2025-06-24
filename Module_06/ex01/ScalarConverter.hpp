63900ec96b59a9b900dc0aa62548acef727642d7
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-06-24 14:54:31 +0200

    started module 6

---
+++ Module_06/ex01/ScalarConverter.hpp
@@ -0,0 +1,13 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <string>
+
+class ScalarConverter {
+	public:
+		static void convert( std::string );
+};
+
+
+#endif // !SCALARCONVERTER_HPP
+
