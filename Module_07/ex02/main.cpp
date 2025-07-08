eb32853490d44418bdd85c6e3df9bc6b5f603bce
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-07-08 16:56:51 +0200

    starting 7/02

---
+++ Module_07/ex02/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "iter.hpp"
+
+void	printInt(int x)
+{
+	std::cout << x << std::endl;
+}
+
+struct Increment {
+	void	operator()(int &x) {
+		x++;
+	};
+};
+
+int main (void)
+{
+	int	arr[] = {1, 3, 4, 6};
+	const int	carr[] = {1, 2, 3, 4};
+	std::cout << "Printing constant array:" << std::endl;
+	iter(carr, 4, printInt);
+	std::cout << "Printing array:" << std::endl;
+	iter(arr, 4, printInt);
+	std::cout << "incrementing array by one" << std::endl;
+	Increment incr;
+	iter(arr, 4, incr);
+	std::cout << "Printing array:" << std::endl;
+	iter(arr, 4, printInt);
+	return 0;
+}
