73225bee63c46ccfc0152291678ea83b92ac6c33
Moat423 <schluesselfehlt@gmail.com>
2025-07-12 16:59:37 +0200

    starting easyfind

---
+++ Module_08/easyfind.hpp
@@ -0,0 +1,5 @@
+// function template easyfind: accepts a type T, takes two parameters:
+// first type T, second an integer.
+// assumes T is container of integers, function finds the first occurrence of the second parameter in the first parameter.
+// If no occurrence is found, you can either throw an exception or return an error value
+// of your choice. If you need some inspiration, analyze how standard containers behave.
