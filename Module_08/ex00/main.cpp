5ad9745bd932839356440d633b37340b86a04b01
Moat423 <schluesselfehlt@gmail.com>
2025-07-14 09:26:55 +0200

    moved easyfind to ex00 and wrote it

---
+++ Module_08/ex00/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+static void printVector(const std::vector<int> &vec)
+{
+	for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+int main ( void )
+{
+	std::vector<int> vec1(5);
+	std::vector<int> vec2(10);
+	std::vector<int>::iterator it;
+	int	value = 3;
+
+	for (int i = 0; i < 10; ++i)
+		vec2[i] = i;
+	std::cout << "printing uninitialized vector:" << std::endl;
+	printVector(vec1);
+	std::cout << "printing initialized vector:" << std::endl;
+	printVector(vec2);
+	std::cout << "finding 3 in vector of integers:" << std::endl;
+	std::cout << "vector contains: " << value << "?" << std::endl;
+	try {
+		it = easyfind(vec1, value);
+		std::cout << "found value at iterator: " << *it << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "value = " << value << e.what() << std::endl;
+		return (1);
+	}
+
+	return (0);
+}
