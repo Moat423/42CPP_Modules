fdaf669d3ed39592b6a149d20c99bb549530b6c0
Moat423 <schluesselfehlt@gmail.com>
2025-07-15 15:20:00 +0200

    changed executable names / rm debug.hpp for subject compliance

--- Module_08/ex01/main.cpp
+++ Module_08/ex01/main.cpp
@@ -52,7 +52,20 @@ void	handleSpans(Span &sp)
 	}
 }
 
-int main ( void )
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
+}
+
+int main_other ( void )
 {
 	srand(time(NULL));
 	Span tenner(10);
